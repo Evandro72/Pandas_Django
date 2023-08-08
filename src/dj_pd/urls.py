0fa128f0cf0fa87b96703a6dddeda606fd72ab55
Evandro72 <evandrobotti@gmail.com>
2023-08-08 14:55:05 -0300

    Merge Tables

--- src/dj_pd/urls.py
+++ src/dj_pd/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('performance/', include('products.urls', namespace='products'))
+    path('performance/', include('products.urls', namespace='products')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
